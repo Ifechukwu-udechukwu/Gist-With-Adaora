2c1ecf17076a2dd4f5427f1be4a02872b1256ae3
ifechukwu-udechukwu <peterifechukwuudechukwu@gmail.com>
2025-08-18 19:27:50 +0100

    started the server side, created routes and models and env

---
+++ server/controllers/blogController.js
@@ -0,0 +1,43 @@
+import fs from "fs"
+import imagekit from "../configs/imageKit.js";
+import Blog from "../models/Blog.js";
+
+export const addBlog = async (req,res)=>{
+    try {
+        const {title, subTitle, description, category, isPublished} = JSON.parse(req.body.blog);
+        const imageFile = req.file;
+
+        // check if all fields are present
+        if(!title || !description || !category || !imageFile){
+            return res.JSON({success: false, message: "missing required fields"});
+        }
+
+        const fileBuffer = fs.readFileSync(imageFile.path)
+
+        // upload image to imageKit
+        const response = await imagekit.upload({
+            file: fileBuffer,
+            fileName: imageFile.originalname,
+            folder: "blogs"
+        })
+
+        //optimization through imageKit URL transformation
+        const optimizedImageUrl = imagekit.url({
+            path: response.filePath,
+            transformation: [
+                {quality: "auto"},
+                {format: "webp"},
+                {width: "1280"}
+            ]
+        });
+
+        const image = optimizedImageUrl;
+
+        await Blog.create({title, subTitle, description, category, image, isPublished})
+
+        res.json({success: true, message: "Blog added successfully"})
+          
+    } catch (error) {
+        res.json({success: false, message: error.message})
+    }
+}
