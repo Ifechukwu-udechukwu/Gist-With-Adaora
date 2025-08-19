3b507b86384f4953693db189888c314775cb5d80
ifechukwu-udechukwu <peterifechukwuudechukwu@gmail.com>
2025-08-19 02:52:32 +0100

    added axios package and created AppContext and AppProvider

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,7 +1,8 @@
 import { useNavigate } from "react-router-dom";
+import { useAppContext } from "../../context/AppContext";
 
 const NavBar = () => {
-    const navigate = useNavigate();
+    const {navigate, token} = useAppContext();
     return (
         <div className="flex justify-between items-center py-3 mx-8 sm:mx-20 xl:mx-32 cursor-pointer">
             <div className="flex items-center" onClick={() => navigate("/")}>
@@ -19,7 +20,7 @@ const NavBar = () => {
                 onClick={() => navigate("/admin")} 
                 className="cursor-pointer rounded-full bg-pink-500 hover:bg-pink-700 text-white px-6 py-2 text-sm sm:text-base transition"
             >
-                Login →
+                {token ? "Dashboard" : "Login →"}
             </button>
         </div>
     );
